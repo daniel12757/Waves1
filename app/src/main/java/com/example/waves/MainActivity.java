e9ab470f9e2e05e71a4e90eddf77a77ca4193e13
danielthewanderingjew <danielthewanderingjew@gmail.com>
2022-07-07 13:38:34 +0300

    seems to be working now which is nice

--- app/src/main/java/com/example/waves/MainActivity.java
+++ app/src/main/java/com/example/waves/MainActivity.java
@@ -7,20 +7,20 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-import com.example.waves.ui.profile.ProfileFragment;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 import com.example.waves.databinding.ActivityMainBinding;
+
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.analytics.FirebaseAnalytics;
 
 
